2fc8dd5b597e70d4215bec7afbfe370fbe820057
inak2002 <kanikannan2002@gmail.com>
2024-01-12 20:57:35 +0530

    ...

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,7 +8,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  user = {
+  register = {
     firstName: '',
     lastName: '',
     email: '',
@@ -19,13 +19,13 @@ export class RegisterComponent {
     // Handle registration logic here
     this.registerUser().subscribe(response =>{    //call back method
       alert("User registration done")
-      console.log('User registered:', this.user);
+      console.log('User registered:', this.register);
     }); 
    
     // You should send this data to the backend for further processing
   }
   registerUser(){
-    var body = {user:this.user}
+    var body = {user:this.register}
     const headers = new HttpHeaders().set('content-type','application/json');
     return this._http.post("https://localhost:44335/api/v1/User/Register",body,{headers})
   }
