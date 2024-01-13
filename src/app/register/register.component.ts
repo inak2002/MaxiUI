1d5a06d140a0c1fada0adce6ab678d29c1aaf6e1
inak2002 <kanikannan2002@gmail.com>
2024-01-13 19:13:34 +0530

    400 error occured

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,24 +8,24 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  register = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: ''
+  user = {
+    FirstName: '',
+    LastName: '',
+    Email: '',
+    Password: ''
   };
   constructor(private _http:HttpClient){}
   onSubmit() {
     // Handle registration logic here
     this.registerUser().subscribe(response =>{    //call back method
       alert("User registration done")
-      console.log('User registered:', this.register);
+      console.log('User registered:', this.user);
     }); 
    
     // You should send this data to the backend for further processing
   }
   registerUser(){
-    var body = {user:this.register}
+    var body = {user:this.user}
     const headers = new HttpHeaders().set('content-type','application/json');
     return this._http.post("https://localhost:44335/api/v1/User/Register",body,{headers})
   }
